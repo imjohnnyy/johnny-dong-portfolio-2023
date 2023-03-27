3c0501c5826be60cf0847302aeb150924fd66ec6
imjohnnyy <ldong4056@gmail.com>
2023-03-27 20:58:43 +1300

    set up routes for navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar"
-
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+        <Home />
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
+import { Link, } from "react-router-dom";
+
 const Navbar = () => {
-    
+
   return (
-    <div>Navbar</div>
+    <nav>
+        <div className="flex items-center justify-between">
+
+            <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
+          
+        </div>
+    </nav>
   )
 }
 
