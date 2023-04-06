da7ddac375c53a90a72e7a18e4f5d01028fc1658
imjohnnyy <ldong4056@gmail.com>
2023-04-06 20:20:45 +1200

    changed profile photo and resized home details

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,25 +3,23 @@ import ProfilePicture from "../assets/images/Johnny_Dong.png"
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 const Home = () => {
-  const flexBetween = "flex items-center";
 
   return (
-    <div className={`mx-auto w-3/6 items-center justify-center`}>
+    <div className={`mx-auto w-3/6 items-center justify-center mt-[280px]`}>
       <div className={"w-3/5 flex item-start mt-40 font-bold text-gray"}>
         <h1 className={"text-6xl text-left"}>Aspiring Software Engineer<img className={" ml-[270px] mt-[-50px] h-[60px] w-[60px]"} src={WavingEmoji} alt="Waving"></img></h1>
      
       </div>
-      <img className={"flex items-end ml-[81.5%]"} src={ProfilePicture} alt="johnny_dong"></img>
-  
-
-        <p className={"flex item-start text-lightgray w-2/4 ml-50 mr-20 mb-20 mt-[-150px] text-left text-lg font-semibold"}>
+      <div className={"flex items-end mt-[-150px]"}>
+        <img className={"ml-[66%] w-[300px] h-[300px]"} src={ProfilePicture} alt="johnny_dong"></img>
+      </div>
+        <p className={"flex item-start text-lightgray w-2/4 ml-50 mr-20 mb-20 mt-[-120px] text-left text-lg font-semibold"}>
         Hi, I'm Johnny Dong. I'm an aspiring Software Engineer based in Auckland,
         New Zealand.  </p>
-        <div className={"w-1/6 ml-[200px] mt-[-110px] fill-red-300"}>
-           <LocationOnIcon />
+        <div className={"w-1/6 ml-[205px] mt-[-110px]"}>
+           <LocationOnIcon style={ {color: '#ef4444'}} />
         </div>
 
-     
     </div>
   );
 };
