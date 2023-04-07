0fe104a330c6095e1e2eb036eaee4ba597f3aeac
imjohnnyy <ldong4056@gmail.com>
2023-04-07 22:19:51 +1200

    added icons for technologies used

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,10 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
+import ReactIcon from "../assets/images/React_Icon.png"
+import JavaScriptIcon from "../assets/images/JavaScript_Icon.png"
+import HTMLIcon from "../assets/images/html_icon.png"
+import CSSIcon from "../assets/images/css_icon.png"
 
 const Home = () => {
 
@@ -24,44 +28,61 @@ const Home = () => {
             <LocationOnIcon style={ {color: '#ef4444'}} />
           </div>
         <div className={"flex item-start w-1/5"}>
-        <LinkedInIcon
-            onClick={() => window.open('https://nz.linkedin.com/in/johnny-dong-', '_blank')}
-            style={{
-              fontSize: 40,
-              color: "#2d2e32",
-              margin: 15,
-              marginLeft: -5,
-              marginTop: 24,
-              cursor: "pointer",
-            }}
-            className={"hover:fill-sky-600"}
-          />
-        <GitHubIcon
-            onClick={() => window.open('https://github.com/imjohnnyy', '_blank')}
-            style={{
-              fontSize: 36,
-              color: "#2d2e32",
-              margin: 15,
-              marginLeft: -5,
-              marginTop: 26,
-              cursor: "pointer",
-            }}
-            className={"hover:fill-sky-600"}
-          />
-          <EmailIcon
-            onClick={() => window.location = 'mailto:imjohnnydong@gmail.com'}
-            style={{
-              fontSize: 43,
-              color: "#2d2e32",
-              margin: 15,
-              marginLeft: -5,
-              marginTop: 23,
-              cursor: "pointer",
-            }}
-            className={"hover:fill-sky-600"}
-          />  
-
-          
+          <LinkedInIcon
+              onClick={() => window.open('https://nz.linkedin.com/in/johnny-dong-', '_blank')}
+              style={{
+                fontSize: 40,
+                color: "#2d2e32",
+                margin: 15,
+                marginLeft: -5,
+                marginTop: 24,
+                cursor: "pointer",
+              }}
+              className={"hover:fill-sky-600"}
+            />
+          <GitHubIcon
+              onClick={() => window.open('https://github.com/imjohnnyy', '_blank')}
+              style={{
+                fontSize: 36,
+                color: "#2d2e32",
+                margin: 15,
+                marginLeft: -5,
+                marginTop: 26,
+                cursor: "pointer",
+              }}
+              className={"hover:fill-sky-600"}
+            />
+            <EmailIcon
+              onClick={() => window.location = 'mailto:imjohnnydong@gmail.com'}
+              style={{
+                fontSize: 43,
+                color: "#2d2e32",
+                margin: 15,
+                marginLeft: -5,
+                marginTop: 23,
+                cursor: "pointer",
+              }}
+              className={"hover:fill-sky-600"}
+            />  
+        </div>
+        <div className={"flex items-center mt-[100px] w-3/4"}>
+            <p className={"text-xl font-semibold text-gray mr-20"}>Front-End</p>
+            <div className="ml-5">
+              <ul className="flex ml-2">
+                <li className="border-2 border-indigo-600 rounded-full w-[68px] h-[68px] bg-white">
+                  <img className={"w-[46px] h-[40px] ml-[8px] mt-[10px] bg-white"} src={ReactIcon} alt="react_icon"></img>
+                </li>
+                <li className="ml-5 border-2 border-indigo-600 rounded-full w-[68px] h-[68px] bg-white">
+                  <img className={"w-[46px] h-[40px] ml-[8px] mt-[10px] bg-white"} src={JavaScriptIcon} alt="javascript_icon"></img>
+                </li>
+                <li className="ml-5 border-2 border-indigo-600 rounded-full w-[68px] h-[68px] bg-white">
+                  <img className={"w-[46px] h-[40px] ml-[8px] mt-[10px] bg-white"} src={HTMLIcon} alt="html_icon"></img>
+                </li>
+                <li className="ml-5 border-2 border-indigo-600 rounded-full w-[68px] h-[68px] bg-white">
+                  <img className={"w-[46px] h-[40px] ml-[8px] mt-[10px] bg-white"} src={CSSIcon} alt="css_icon"></img>
+                </li>
+            </ul>
+            </div>
         </div>
       </div>
     </div>
