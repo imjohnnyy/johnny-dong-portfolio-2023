de4de50068f79abe46b0ed23b3ecb3211fc2c8f4
imjohnnyy <ldong4056@gmail.com>
2023-04-14 18:42:53 +1200

    Footer is now responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,13 @@ import EmailIcon from "@mui/icons-material/Email";
 
 const Footer = () => {
   return (
-    <div className={"bg-gray flex items-center justify-between h-[130px]"}>
-      <div className={"flex items-center mx-auto w-1/2"}>
-        <p className={"text-white text-lg font-bold"}>
-          {" "}
+    <div className={"bg-gray flex items-center justify-between max-md:py-0 py-5 "}>
+      <div className={"flex items-center mx-auto max-lg:flex-col max-lg:w-2/3 max-lg:mt-auto"}>
+        <p className={"text-white text-lg font-bold max-sm:mt-[3rem]"}>
+
           Copyright &copy; 2023 Johnny Dong
         </p>
-        <div className={"ml-[500px] float-right"}>
+        <div className={"lg:ml-[500px] max-lg:flex max-lg:items-center max-lg:justify-center max-lg:h-8 max-lg:mt-8 max-lg:mb-10"}>
           <LinkedInIcon
             onClick={() =>
               window.open("https://nz.linkedin.com/in/johnny-dong-", "_blank")
