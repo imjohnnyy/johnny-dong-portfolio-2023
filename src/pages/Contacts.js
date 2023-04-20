7fb3bb1f3dd27dc6f2f3241d1b0cfd9e196e7354
imjohnnyy <ldong4056@gmail.com>
2023-04-20 21:02:48 +1200

    contact page is responsive and complete

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -7,39 +7,39 @@ const Contacts = () => {
         <div className="bg-lightergray w-auto flex items-center justify-center mx-auto" id="contact">
           <div className="flex flex-col items-start text-left w-2/8 mx-8 my-[6rem] max-md:w-[90%]">
             <h2 className="font-black text-lg text-blue-500 w-1/2 max-lg:text-center max-lg:mx-auto">CONTACT</h2>
-            <h3 className="font-bold text-gray text-2xl mt-2 max-md:text-center max-md:justify-center">
+            <h3 className="font-bold text-gray text-2xl mt-2 max-md:text-center max-md:justify-center max-lg:text-center max-lg:mx-auto">
               Get in touch with me! 👇
             </h3>
 
             {/* Circle with Icon */}
-            <div className="flex flex-row mt-12">
-                <div className="flex w-1/2">
+            <div className="flex flex-row mt-12 max-md:flex-col max-lg:items-center max-lg:justify-center max-lg:mx-auto">
+                <div className="flex w-1/2 max-lg:flex-col max-lg:items-center">
                     <div class="flex h-20 w-20 items-center justify-center rounded-full bg-white shadow-lg">
                         <MapIcon style={{fontSize: 42, color: "#3b82f6", }}/>
                     </div>
                     <div className="mx-6 my-4 ">
-                        <h2 className="text-gray font-bold text-lg">Location</h2>
-                        <p className="text-lightgray font-medium text-medium whitespace-nowrap">Auckland, New Zealand</p>
+                        <h2 className="text-gray font-bold text-lg max-lg:text-center max-lg:mx-auto">Location</h2>
+                        <p className="text-lightgray font-medium text-medium whitespace-nowrap max-lg:text-center max-lg:mx-auto max-lg:mb-10">Auckland, New Zealand</p>
                     </div>
                 </div>
 
-                <div className="flex w-1/2">
+                <div className="flex w-1/2 max-lg:flex-col max-lg:items-center">
                     <div class="flex h-20 w-20 items-center justify-center rounded-full bg-white shadow-lg">
                         <EmailIcon style={{fontSize: 40, color: "#3b82f6", }}/>
                     </div>
                     <div className="mx-6 my-4">
-                        <h2 className="text-gray font-bold text-lg">Email</h2>
-                        <p className="text-lightgray font-medium text-medium whitespace-nowrap">imjohnnydong@gmail.com</p>
+                        <h2 className="text-gray font-bold text-lg max-lg:text-center max-lg:mx-auto">Email</h2>
+                        <p className="text-lightgray font-medium text-medium whitespace-nowrap max-lg:text-center max-lg:mx-auto max-lg:mb-10">imjohnnydong@gmail.com</p>
                     </div>
                 </div>
 
-                <div className="flex w-1/2">
+                <div className="flex w-1/2 max-lg:flex-col max-lg:items-center">
                     <div class="flex h-20 w-20 items-center justify-center rounded-full bg-white shadow-lg">
                         <CallIcon style={{fontSize: 40, color: "#3b82f6", }}/>
                     </div>
                     <div className="mx-6 my-4">
-                        <h2 className="text-gray font-bold text-lg">Phone</h2>
-                        <p className="text-lightgray font-medium text-medium whitespace-nowrap">(+64) 022 650 2893</p>
+                        <h2 className="text-gray font-bold text-lg max-lg:text-center max-lg:mx-auto">Phone</h2>
+                        <p className="text-lightgray font-medium text-medium whitespace-nowrap max-lg:content-center max-lg:mx-auto max-lg:mb-10">(+64) 022 650 2893</p>
                     </div>
                 </div>
 
